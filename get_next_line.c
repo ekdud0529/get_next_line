e1346e7af6ce901c1535c5c2dcbad63d3bdd2fd3
daykim <45174177+ekdud0529@users.noreply.github.com>
2022-03-17 21:36:16 +0900

    add code

--- get_next_line.c
+++ get_next_line.c
@@ -14,13 +14,49 @@
 
 // 1. file open
 // 2. buf size 만큼 읽고 저장
+// -> size 보다 적게 남아서 에러 발생시? errno
 // 3. buf에서 개행문자 찾기
 // 4. 개행문자 있으면 거기까지 리턴 없으면 저장
 // 5. file 다 읽을때까지  2 ~ 4 반복
 // 6. 파일 다 읽었으면 3 ~ 4 반복
 
+int	find_newline(char *buf)
+{
+	int	index;
+
+	index = 0;
+	while (*(buf + index))
+	{
+		if ((*buf == '\n') || (*buf == '\0'))
+		{
+			return (index);
+		}
+	}
+	return (-1);
+}
+
+char	*get_str(int fd)
+{
+	char	*buf;
+	int		find;
+
+	find = -1;
+	while (find < 0)
+	{
+		buf = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+		buf = read(fd, *buf, BUFFER_SIZE);
+		find = find_newline(buf);
+		if(find >= 0)
+		{
+			
+		}
+	}
+}
+
 char	*get_next_line(int fd)
 {
-	
+	static char	*str_save;
+
+	str_save = get_str(fd);
 	return ;
 }
